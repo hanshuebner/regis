221b2e9ceefb22f0ec2ee3c06ee0fa97e5a9e4b7
Hans Hübner <hans.huebner@gmail.com>
2024-02-07 14:15:11 +0100

    emulator tweaks

--- emulate.sh
+++ emulate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd emulate
-mame vt240 -window -host null_modem -bitb socket.127.0.0.1:11313 -r 800x480 -skip_gameinfo
+mame vt240 -window -host null_modem -bitb socket.127.0.0.1:11313 -r 800x480 -skip_gameinfo -inipath .
